924ee507563a23a9eb53b8eea978ada2d9d92544
Stefania Giannina Arias Farias <stefaniaarias@Stefanias-MacBook-Pro.local>
2023-04-24 19:53:00 -0300

    Adding paginated list for restaurant cards

---
+++ src/components/RestaurantListContainer.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import { IRestaurantCardList } from "../types";
+import { RestaurantCardList } from "./RestaurantCardList";
+import './RestaurantCardListContainer.scss';
+
+export function RestaurantCardListContainer(props:IRestaurantCardList){
+    const [currentPage, setCurrentPage] = useState(1);
+
+    const pageNumberLimit = 10;
+    const totalPages = Math.ceil(props.restaurantCardList.length / pageNumberLimit ) ;
+    const isDisabledNextButton = () => currentPage >= totalPages;
+    const isDisabledPrevButton = () => currentPage === 1;
+
+    const itemFrom = (currentPage-1) * pageNumberLimit;
+    const itemTo = (currentPage * pageNumberLimit)-1;
+    const restaurantListToDisplay =props.restaurantCardList.slice(itemFrom, itemTo);
+    
+    return (
+        <div>
+            <RestaurantCardList restaurantCardList={restaurantListToDisplay}/>
+            <div>
+                <button 
+                    className="pagination__button"
+                    onClick={()=> setCurrentPage(currentPage-1)}
+                    disabled={isDisabledPrevButton()}
+                >
+                    Prev
+                </button>
+                <button 
+                    className="pagination__button"
+                    onClick={()=> setCurrentPage(currentPage+1)}
+                    disabled={isDisabledNextButton()}
+                >
+                    Next
+                </button>
+
+            </div>
+            <p>Page {currentPage} of {totalPages}</p>
+        </div>
+    );
+}
